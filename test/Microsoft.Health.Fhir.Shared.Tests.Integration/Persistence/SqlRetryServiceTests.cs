72b300ff6baa0786c8b2181fbc9fe25627abf01b
Brendan Kowitz <bkowitz@microsoft.com>
2024-04-15 17:04:04 -0700

    Subscription infra

--- test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/SqlRetryServiceTests.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/SqlRetryServiceTests.cs
@@ -377,7 +377,7 @@ END
 
                 using var sqlCommand = new SqlCommand();
                 sqlCommand.CommandText = $"dbo.{storedProcedureName}";
-                var result = await sqlRetryService.ExecuteReaderAsync<long, SqlRetryService>(
+                var result = await sqlRetryService.ExecuteReaderAsync<long>(
                     sqlCommand,
                     testConnectionInitializationFailure ? ReaderToResult : ReaderToResultAndKillConnection,
                     logger,
@@ -415,7 +415,7 @@ END
                 try
                 {
                     _output.WriteLine($"{DateTime.Now:O}: Start executing ExecuteSqlDataReader.");
-                    await sqlRetryService.ExecuteReaderAsync<long, SqlRetryService>(
+                    await sqlRetryService.ExecuteReaderAsync<long>(
                         sqlCommand,
                         testConnectionInitializationFailure ? ReaderToResult : ReaderToResultAndKillConnection,
                         logger,
