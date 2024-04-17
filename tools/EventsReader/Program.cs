74561531b98f79c13146259b9c7c44963b31c080
Fernando Henrique Inocêncio Borba Ferreira <fernfe@microsoft.com>
2024-04-17 11:41:20 -0700

    DataLakeChannel.

--- tools/EventsReader/Program.cs
+++ tools/EventsReader/Program.cs
@@ -15,13 +15,13 @@ namespace Microsoft.Health.Internal.Fhir.EventsReader
     {
         private static readonly string _connectionString = ConfigurationManager.ConnectionStrings["Database"].ConnectionString;
         private static SqlRetryService _sqlRetryService;
-        private static SqlStoreClient<SqlServerFhirDataStore> _store;
+        private static SqlStoreClient _store;
 
         public static void Main()
         {
             ISqlConnectionBuilder iSqlConnectionBuilder = new Sql.SqlConnectionBuilder(_connectionString);
             _sqlRetryService = SqlRetryService.GetInstance(iSqlConnectionBuilder);
-            _store = new SqlStoreClient<SqlServerFhirDataStore>(_sqlRetryService, NullLogger<SqlServerFhirDataStore>.Instance);
+            _store = new SqlStoreClient(_sqlRetryService, NullLogger<SqlStoreClient>.Instance);
 
             var totalsKeys = 0L;
             var totalTrans = 0;
